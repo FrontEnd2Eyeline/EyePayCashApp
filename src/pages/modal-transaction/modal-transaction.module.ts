711c047055ca83e0f23950f41548745c3fbff115
andresantos <andresrodriguezsantos44@hotmail.com>
2018-10-06 11:34:53 -0500

    Transaccion

--- src/pages/modal-transaction/modal-transaction.module.ts
+++ src/pages/modal-transaction/modal-transaction.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ModalTransactionPage } from './modal-transaction';
+import {BrMaskerModule} from "brmasker-ionic-3";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ModalTransactionPage } from './modal-transaction';
   ],
   imports: [
     IonicPageModule.forChild(ModalTransactionPage),
+    BrMaskerModule,
   ],
 })
 export class ModalTransactionPageModule {}
