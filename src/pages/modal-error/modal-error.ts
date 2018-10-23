22a3fa70e92c7e4a94a4e1a9d3e53c0575ac33e4
eRendonC <alexanderendon@hotmail.com>
2018-10-23 17:03:09 -0500

    Created provider and modal error

---
+++ src/pages/modal-error/modal-error.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ModalErrorPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-modal-error',
+  templateUrl: 'modal-error.html',
+})
+export class ModalErrorPage {
+public obj : any = {field:null, message:null};
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+	 this.obj = this.navParams.get('Error');
+    console.log(this.obj);
+  }
+
+  closeModal() {
+    this.navCtrl.pop();
+  }
+
+}
