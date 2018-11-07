6721e050c4067b5b20634dc0d2ee063b4e4640a9
andresantos <andresrodriguezsantos44@hotmail.com>
2018-11-07 08:20:55 -0500

    API

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -10,8 +10,6 @@ import {LoginPage} from "../pages/login/login";
 @Injectable()
 export class Api {
   url: string = 'http://globalone2onemarketing.com/frontend/web/apiapp/';
-  // url: string = 'http://10.160.170.114/eyepaycash/frontend/web/apiapp/';
-  // url: string = 'http://35557b72.ngrok.io/eyepaycash/frontend/web/apiapp/';
   headers: HttpHeaders;
 
   constructor(public http: HttpClient,
